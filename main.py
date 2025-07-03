6e79186fdd461e58d6ec65d30fc7ce1b0b0ac826
Sajadrrtt <sjad26107@gmail.com>
2025-07-03 22:15:16 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import telebot
 
+TOKEN = "7871752923:AAFpNYXOdGFzIR-QYmjuQr3wtp49cO8UuwM"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "أهلاً بيك، البوت شغال 🔥")
+
+@bot.message_handler(func=lambda m: True)
+def echo(message):
+    bot.reply_to(message, f"قلت: {message.text}")
+
+bot.infinity_polling()
